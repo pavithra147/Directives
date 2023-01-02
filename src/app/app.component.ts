c24550b2b62729366468ca00d42d2a745d8e50f7
pavithra147 <pavithra.jayavel@aspiresys.com>
2023-01-02 17:43:01 +0530

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directiveImplements';
+  EmployeeLists: any;
+  EmployeeList=[{
+    name:'Yamuna',
+    empid :101,
+    domain: "Java",
+    Gender:'f' 
+  },
+  {
+    name:"Ravi",
+    empid :102,
+    domain: "Dotnet",
+    Gender:'m'
+  }
+]
+
 }
